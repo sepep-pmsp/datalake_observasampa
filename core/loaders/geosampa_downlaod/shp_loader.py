a7704f344cecb7de3dc90b893d18fb7b6e60227e
h-pgy <hpougy@gmail.com>
2023-02-27 16:39:09 -0300

    refactor to return the geodf and not save file

--- core/loaders/geosampa_downlaod/shp_loader.py
+++ core/loaders/geosampa_downlaod/shp_loader.py
@@ -1,5 +1,8 @@
+from io import BytesIO
+from tempfile import TemporaryDirectory
+import geopandas as gpd
 
-from core.utils.file_path import solve_dir, solve_path, check_dir_exists, list_files_recursive, delete_existing_files
+from core.utils.file_path import list_files_recursive
 from core.utils.io import download_binary_file, unzip_file
 
 
@@ -8,53 +11,57 @@ class ShpDownloader:
     
     domain = 'http://download.geosampa.prefeitura.sp.gov.br/PaginasPublicas/'
 
-    def get_file_uri(self, fname:str)->str:
 
-        uri = f'downloadArquivo.aspx?orig=DownloadCamadas&arq={fname}&arqTipo=Shapefile'
+    def __init__(self):
+
+        self.tmp_folder = TemporaryDirectory()
+        
+    def get_file_uri(self, camada_name:str)->str:
+
+        uri = f'downloadArquivo.aspx?orig=DownloadCamadas&arq={camada_name}&arqTipo=Shapefile'
 
         return self.domain + uri
     
-    def download_shp_zip(self, fname:str, zip_folder:str)->str:
+    def download_shp_zip(self, uri:str)->BytesIO:
 
-        uri = self.get_file_uri(fname)
-        fname = solve_path(f'{fname}.zip', parent=zip_folder)
+        content = download_binary_file(uri)
 
-        download_binary_file(uri, fname)
+        return BytesIO(content)
+    
+    def unzip_to_tmp_folder(self, zipped_content:BytesIO)->None:
 
-        return fname
+        unzip_file(zipped_content, self.tmp_folder.name)
     
     
-    def unzip_shp(self, fname:str, zip_folder:str, shp_folder:str)->None:
+    def get_shape_path(self)->str:
 
-        if fname.endswith('.zip'):
-            fname = fname[:-4]
-        fname = solve_path(f'{fname}.zip', parent=zip_folder)
+        shps = list_files_recursive(self.tmp_folder.name, '.shp')
 
-        unzip_file(fname, shp_folder)
+        return shps[0]
+    
+    def open_shp(self, shp_path:str)->gpd.GeoDataFrame:
 
+        return gpd.read_file(shp_path)
+    
+    def empty_folder(self):
 
-    def check_shp_exists(self, shp_folder:str)->bool:
+        self.tmp_folder.cleanup()
+
+    def pipe_download_shp(self, camada_name:str)->gpd.GeoDataFrame:
+
+        uri = self.get_file_uri(camada_name)
+        zip_bytes = self.download_shp_zip(uri)
+        self.unzip_to_tmp_folder(zip_bytes)
+
+        shp_path = self.get_shape_path()
+        print(shp_path)
+        geodf = self.open_shp(shp_path)
         
-        checagem = check_dir_exists(shp_folder)
-        if checagem:
-            shp = list_files_recursive(shp_folder, '.shp')
-            if shp:
-                return True
-        return False
+        #self.empty_folder()
 
-    def pipe_download_shp(self, fname:str, shp_folder:str, zip_folder:str, check=True)->None:
+        return geodf
 
-        if check:
-            checagem = self.check_shp_exists(shp_folder)
-            if checagem:
-                print(f'Shape {shp_folder} já salvo')
-                return shp_folder
 
-        zip_path = self.download_shp_zip(fname, zip_folder)
+    def __call__(self, camada_name:str)->None:
 
-        self.unzip_shp(zip_path, zip_folder, shp_folder)
-
-    
-    def __call__(self, fname:str, shp_folder:str, zip_folder:str, check_already_exists=True)->None:
-
-        self.pipe_download_shp(fname, shp_folder, zip_folder, check_already_exists)
+        return self.pipe_download_shp(camada_name)
